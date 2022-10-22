7793ab250fa03fa2b6ac26ded699cd1b18b76935
Argadiuz <arkadiusz.zdziech@o2.pl>
2022-10-22 13:54:48 +0200

    Added initial project structure for Notery;

---
+++ Documentation/Manual/SphinxDocs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Notery'
+copyright = '2022, Arkadiusz Zdziech'
+author = 'Arkadiusz Zdziech'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'English'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'groundwork'
+html_static_path = ['_static']
---
+++ Documentation/Manual/SphinxDocs/source/index.rst
@@ -0,0 +1,20 @@
+.. Armida.Notery.Manual documentation master file, created by
+   sphinx-quickstart on Sat Oct 22 13:41:58 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Notery's documentation!
+================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
